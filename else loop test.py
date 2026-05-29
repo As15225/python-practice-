0571cbf9e7148ecd4eb7775ffb33bb81eb238936
Asispal <asispal123singh@gmail.com>
2026-05-29 12:17:36 +0530

    ATM pin verify code

--- else loop test.py
+++ else loop test.py
@@ -1,1 +1,6 @@
-print("HEllo world")
+Correct_pin=2727
+Enter_pin=int(input("Enter Your PIN = "))
+while Enter_pin!=Correct_pin:
+       print("///WRONG PIN, ACCESS DENIED///")
+       Enter_pin=int(input("Wrong Pin, Try again please = "))
+       print("PIN Verified. Welcome User")
