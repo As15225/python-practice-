b6970d74909b441489b3af38f7eeefb9333daecf
Asispal <asispal123singh@gmail.com>
2026-06-02 09:48:56 +0530

    2june

--- loop activity.py
+++ loop activity.py
@@ -20,10 +20,38 @@
 #         number=number + 1
 #         print(number)
 
-counter = 7
-sum = 10
-while counter <=7:
-    sum = sum + counter
-    counter = counter + 2
-print(sum)
+# counter = 7
+# sum = 10
+# while counter <=7:
+#     sum = sum + counter
+#     counter = counter + 2
+# print(sum)
+
+
+# n = 5
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end="")
+#     print()
+
+# n=5
+# for i in range (n,0,-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+ 
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range (1,n+1):
+#         print(j,end="")
+#     print()
+
+n=5
+for i in range (1,n):
+    for j in range(n,0,-1):
+        print(j,end="")
+    print()
+
+
 
